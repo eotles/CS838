2cc986a642c4b8b639d284b8d08a2be73bb1c783
eotles <eotles@gmail.com>
2015-03-31 12:20:02 -0500

    commenting

--- parser.py
+++ parser.py
@@ -4,12 +4,18 @@ Created on Mar 30, 2015
 @author: hliu, tjaraczewski,eotles
 '''
 
+#ParsedData Class
+#Attributes:
+#    samples - list of sample IDs (corresponding to rows of matrix)
+#    names - list of column names (corresponding to columns of matrix + 1)
+#    matrix - list of lists storing the data for each sample
 class ParsedData(object):
+    #Constructor
     def __init__(self, samples, names, matrix):
         self.samples = samples
         self.names = names
         self.matrix = matrix
-    
+    #
     def disp(self, show_matrix=False):
         print("%s samples: %s\n%s names: %s" %(len(self.samples), self.samples, len(self.names), self.names))
         print("matrix size: %s x %s" %(len(self.matrix),len(self.matrix[0])))
@@ -18,7 +24,11 @@ class ParsedData(object):
             for row in self.matrix:
                 print(row)
 
-#parsing function to take file and return ParsedData object
+#TCGA data parsing function
+#Parameters:
+#    filename - the input data filepath
+#Returns:
+#    ParsedData object
 def tcga(filename):
     with open(filename,'r') as f:
         content = [x.strip('\n') for x in f.readlines()]
@@ -28,6 +38,18 @@ def tcga(filename):
         samples = []
         data_matrix = []
         for idx,line in enumerate(content):
+            if idx >= 1:
+                line_data = line.split('\t')
+                samples.append(line_data[0])
+                data_matrix.append([float(x) for x in line_data[1:]]) 
+        return(ParsedData(samples, names, data_matrix))
+
+def ccle(filename):
+    with open(filename, 'r') as f:
+        names = []
+        samples = []
+        data_matrix = []
+        for idx,line in enumerate(f):
             if idx >= 1:
                 line_data = line.split('\t')
                 samples.append(line_data[0])
--- test.py
+++ test.py
@@ -3,10 +3,10 @@ Created on Mar 30, 2015
 
 @authors: hliu, tjaraczewski, eotles
 '''
-import parser as prs
 
+#Testing the functionality of the TCGA parser
+import parser as prs
 data = {_: dict() for _ in ["PRAD"]}
 data["PRAD"]["TCGA"] = prs.tcga('Data/TCGA/PRAD/PRAD.uncv2.mRNAseq_raw_counts.txt') 
-
 data["PRAD"]["TCGA"].disp()
 
