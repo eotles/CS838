9d7073afeff01221e32844b29b0d92fd7cf9888f
Haixiang Liu <hliu253@wisc.edu>
2015-04-09 23:13:38 -0500

    adding pca

--- test.py
+++ test.py
@@ -10,10 +10,12 @@ from mpl_toolkits.mplot3d import Axes3D
 from sklearn.cluster import KMeans
 from scipy.spatial.distance import pdist, squareform
 from scipy.cluster.hierarchy import linkage, dendrogram
-
+from Algorithms import *
+import scipy.io
 def main():
     #Testing the functionality of the TCGA parser
     import parser as prs
+
     print("Loading data....")
     #The TCGA file below jut has data for prostate cancer - right?
     prad_tcga_filepath = 'Data/TCGA/PRAD/PRAD.uncv2.mRNAseq_raw_counts.txt'
@@ -21,34 +23,45 @@ def main():
     ccle_filepath = 'Data/CCLE/CCLE_Expression_Entrez_2012-09-29.txt'
     data = {_: dict() for _ in ["PRAD"]}
     data["PRAD"]["TCGA"] = prs.tcga(prad_tcga_filepath)
+    scipy.io.savemat('samples.mat', mdict={'samples': data["PRAD"]["TCGA"] .samples})
+    scipy.io.savemat('genes.mat', mdict={'genes': data["PRAD"]["TCGA"] .names})
+
+    print("Runing PCA....")
+    pca = PCA_wrapper(data["PRAD"]["TCGA"].matrix,3)
+    pca_data = pca.transform(data["PRAD"]["TCGA"].matrix);
+    scipy.io.savemat('pca_data_3d.mat', mdict={'pca_data_3d': pca_data})
+    
+    print("Runing KMeans....")
+    labels = KMeans_wrapper(pca_data,8)
+    scipy.io.savemat('labels.mat', mdict={'labels': labels})
     #data["PRAD"]["TCGA"].disp()
     
     #data["CCLE"] = prs.ccle(ccle_filepath)
     #data["CCLE"].disp()
     #print(data["PRAD"]["CCLE"].names[0])
-    print("Computing Distance Matrix")
-    X = data["PRAD"]["TCGA"].matrix[:100]
-    distanceMatrix = pdist(X)
+    #print("Computing Distance Matrix")
+    #X = data["PRAD"]["TCGA"].matrix[:100]
     
-    estimators = {'k_means_iris_3': KMeans(n_clusters=3),
-        'k_means_iris_8': KMeans(n_clusters=8),
-            'k_means_iris_bad_init': KMeans(n_clusters=3, n_init=1,
-                                            init='random')}
-    for name, est in estimators.items():
-        print(name)
-        est.fit(X)
-        labels = est.labels_
-        actlabels = labels
+    #estimators = {'k_means_iris_3': KMeans(n_clusters=3),
+    #'k_means_iris_8': KMeans(n_clusters=8),
+    #'k_means_iris_bad_init': KMeans(n_clusters=3, n_init=1,
+    #init='random')}
 
-        print("ploting......")
-        dendrogram(linkage(distanceMatrix, method='complete'),
-                   color_threshold=0.3,
-                   leaf_label_func=lambda x: 'O' * (actlabels[x] + 1),
-                   leaf_font_size=6)
+    #for name, est in estimators.items():
+    #print(name)
+    #est.fit(X)
+    #labels = est.labels_
+    #actlabels = labels
+    #distanceMatrix = pdist(X)
+    #print("ploting......")
+    #dendrogram(linkage(distanceMatrix, method='complete'),
+    #color_threshold=0.3,
+    #leaf_label_func=lambda x: data["PRAD"]["TCGA"] .samples[x],
+    #leaf_font_size=6)
     
-        f = plt.gcf()
-        f.set_size_inches(8, 4);
-        print("Showing plt.....")
-        plt.show()
+    #f = plt.gcf()
+    #f.set_size_inches(8, 4);
+    #print("Showing plt.....")
+    #plt.show()
 if __name__ == '__main__':
     main()
